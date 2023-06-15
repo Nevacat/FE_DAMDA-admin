5fdc39d3a9561328a413548d237883c79288d89e
PYO SEUNGYEON <pyo.seungyeonn@gmail.com>
2023-06-16 00:17:49 +0900

    design: 간격 조정

--- src/components/manager/ManagerTable/style.ts
+++ src/components/manager/ManagerTable/style.ts
@@ -1,6 +1,10 @@
 import { Th } from '@/styles/common/table.style';
 import styled from '@emotion/styled';
 
+export const ManagerTableContainer = styled.div`
+  margin-bottom: 40px;
+`;
+
 export const ManagerTh = styled(Th)`
   padding: 0 12px;
 `;
