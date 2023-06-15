8ee6ae83eb9e42cd3a056c967fb80bd8f8efbe81
PYO SEUNGYEON <pyo.seungyeonn@gmail.com>
2023-06-15 16:00:21 +0900

    design: 테이블 공통 스타일 변경
    * th, tr, td, thead

--- src/styles/common/table.style.ts
+++ src/styles/common/table.style.ts
@@ -2,22 +2,34 @@ import styled from '@emotion/styled';
 import theme from '../theme';
 
 export const TableContainer = styled.div``;
+
 export const Table = styled.table`
   width: 100%;
 `;
-export const Thead = styled.thead``;
+
+export const Thead = styled.thead`
+  border-bottom: 1px solid #eeeff2;
+`;
+
 export const Tbody = styled.tbody``;
-export const Tr = styled.tr``;
+
+export const Tr = styled.tr`
+  border-bottom: 1px solid #eeeff2;
+`;
+
 export const Th = styled.th`
-  padding: 2px 4px;
+  height: 72px;
+  padding: 0 12px;
   color: ${theme.colors.yolda_gray_3};
   font-size: 14px;
-  line-height: 42px;
-  text-align: center;
+  text-align: left;
   white-space: nowrap;
+  vertical-align: middle;
 `;
+
 export const Td = styled.td`
-  padding: 2px 4px;
-  text-align: center;
+  height: 80px;
+  padding: 0 12px;
   word-break: keep-all;
+  vertical-align: middle;
 `;
