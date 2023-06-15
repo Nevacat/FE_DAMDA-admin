1bfd2dd119e9c52609b6ec76f5a53714a594555b
PYO SEUNGYEON <pyo.seungyeonn@gmail.com>
2023-06-15 15:51:29 +0900

    design: 테이블 헤더 영역 구현

--- src/components/manager/ManagerLayout/index.tsx
+++ src/components/manager/ManagerLayout/index.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+
+import * as G from '@/styles/common/table.style';
 import * as S from './style';
 
 function ManagerLayout() {
@@ -7,6 +9,26 @@ function ManagerLayout() {
       <S.Header className="header">
         <h1>매니저 관리</h1>
       </S.Header>
+
+      <div className="content">
+        <G.Table>
+          <G.Thead>
+            <tr>
+              <G.Th scope="col">이름</G.Th>
+              <G.Th scope="col">연락처</G.Th>
+              <G.Th scope="col">거주지</G.Th>
+              <G.Th scope="col">활동지역</G.Th>
+              <G.Th scope="col">레벨</G.Th>
+              <G.Th scope="col">자격증</G.Th>
+              <G.Th scope="col">운전여부</G.Th>
+              <G.Th scope="col">지원 폼</G.Th>
+              <G.Th scope="col">예약 내역</G.Th>
+              <G.Th scope="col">상태</G.Th>
+              <G.Th scope="col">메모</G.Th>
+            </tr>
+          </G.Thead>
+        </G.Table>
+      </div>
     </div>
   );
 }
