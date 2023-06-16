9aedb292ab8e5ed1df9cfce1a100c7ef3dc5c531
seunghye <tmdgp0212@naver.com>
2023-06-16 20:16:28 +0900

    fix: theme type 복구

--- src/styles/emotion.d.ts
+++ src/styles/emotion.d.ts
@@ -3,11 +3,74 @@ import '@emotion/react';
 declare module '@emotion/react' {
   export interface Theme {
     colors: {
-      [name: sting]: string;
+      yolda_black_1: string;
+      main_blue: string;
+      sub_blue_1: string;
+      sub_blue_2: string;
+      sub_blue_3: string;
+      sub_blue_4: string;
+      red_1: string;
+      green_1: string;
+      yellow_1: string;
+      yolda_gray_1: string;
+      yolda_gray_2: string;
+      yolda_gray_3: string;
+      yolda_gray_4: string;
+      yolda_gray_5: string;
     };
 
     font: {
-      [name: string]: {
+      bold_30: {
+        'font-size': string;
+        'font-weight': string;
+        'line-height': string;
+      };
+      bold_30_135: {
+        'font-size': string;
+        'font-weight': string;
+        'line-height': string;
+      };
+      bold_24: {
+        'font-size': string;
+        'font-weight': string;
+        'line-height': string;
+      };
+      bold_24_135: {
+        'font-size': string;
+        'font-weight': string;
+        'line-height': string;
+      };
+      bold_19: {
+        'font-size': string;
+        'font-weight': string;
+        'line-height': string;
+      };
+      bold_18_135: {
+        'font-size': string;
+        'font-weight': string;
+        'line-height': string;
+      };
+      bold_16: {
+        'font-size': string;
+        'font-weight': string;
+        'line-height': string;
+      };
+      bold_14: {
+        'font-size': string;
+        'font-weight': string;
+        'line-height': string;
+      };
+      regular_14: {
+        'font-size': string;
+        'font-weight': string;
+        'line-height': string;
+      };
+      regular_12: {
+        'font-size': string;
+        'font-weight': string;
+        'line-height': string;
+      };
+      light_14: {
         'font-size': string;
         'font-weight': string;
         'line-height': string;
