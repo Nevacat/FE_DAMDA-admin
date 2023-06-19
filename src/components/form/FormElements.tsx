46a560cee409daaf70ad347e8ad60b392875e22d
BeeMOre32 <windowssart01@gmail.com>
2023-06-19 13:18:37 +0900

    feat: radio 버튼과 연동

--- src/components/form/FormElements.tsx
+++ src/components/form/FormElements.tsx
@@ -3,17 +3,23 @@ import { FormElementsProps } from '@/types/components/form';
 import Title from '@/components/form/components/Title';
 import AddressInput from '@/components/form/components/AddressInput';
 import Select from '@/components/form/components/Select';
+import Radio from '@/components/form/components/Radio';
 
 function FormElements({ formData }: FormElementsProps) {
+  if (formData.questionIdentify === 'ADDRESS') return <AddressInput formData={formData} />;
+
   switch (formData.questionType) {
     case 'TITLE':
       return <Title formData={formData} />;
     case 'STRING':
-      return <div>문자열</div>;
+      return <Title formData={formData} />;
     case 'ADDRESS':
       return <AddressInput formData={formData} />;
     case 'SELECT':
       return <Select formData={formData} />;
+    case 'RADIO':
+      return <Radio formData={formData} />;
+    case 'DATE':
   }
 }
 
