6ef16afa38fa91c3d782f3f4aef1f1b3a0c6c24a
PYO SEUNGYEON <pyo.seungyeonn@gmail.com>
2023-06-20 15:47:29 +0900

    refactor: 상태별 매니저 데이터 API로 변경

--- src/api/manager.ts
+++ src/api/manager.ts
@@ -1,6 +1,6 @@
 import { instance } from './instance';
 
-export const getActiveManagers = async () => {
-  const response = await instance.get('/admin/manager/active');
+export const getManagers = async (status: string) => {
+  const response = await instance.get(`/admin/manager?status=${status}`);
   return response.data;
 };
