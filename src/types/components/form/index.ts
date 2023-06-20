dce2d37f014e6d194b9beab5c22cdebd78c11e4a
BeeMOre32 <windowssart01@gmail.com>
2023-06-20 19:23:08 +0900

    feat: mutation 이 일어나면 refetch 를 유도 하도록 로직 추가

--- src/types/components/form/index.ts
+++ src/types/components/form/index.ts
@@ -3,33 +3,41 @@ import React from 'react';
 
 export interface FormElementsProps {
   formData: AdminForm;
+  refetch: () => void;
 }
 
 export interface FormInputProps {
   formData: AdminForm;
+  refetch: () => void;
 }
 
 export interface FormAddressProps {
   formData: AdminForm;
+  refetch: () => void;
 }
 
 export interface FormSelectProps {
   formData: AdminForm;
+  refetch: () => void;
 }
 
 export interface FormRadioProps {
   formData: AdminForm;
   children: React.ReactNode | null;
+  refetch: () => void;
 }
 
 export interface FormDateInputProps {
   formData: AdminForm;
+  refetch: () => void;
 }
 
 export interface FormInputProps {
   formData: AdminForm;
+  refetch: () => void;
 }
 
 export interface FormChangeButtonProps {
   formData: AdminForm;
+  refetch: () => void;
 }
