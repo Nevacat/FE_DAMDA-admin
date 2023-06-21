8b2771750fd56662851b5f8c6215c1a37b81e1e4
PYO SEUNGYEON <pyo.seungyeonn@gmail.com>
2023-06-21 15:44:28 +0900

    feat: 데이터 수정 기능 (임시)
    * 이름, 연락처, 활동 지역, 레벨, 자격중, 운전 여부

--- src/components/manager/ManagerTable/ManagerItem/index.tsx
+++ src/components/manager/ManagerTable/ManagerItem/index.tsx
@@ -1,15 +1,48 @@
 import React, { useState } from 'react';
 
+import LocationSelectionForm from '../../LocationSelectionForm';
+
 import * as G from '@/styles/common/table.style';
 import { StateButton } from '@/styles/common/StateButton';
 import * as S from './style';
 
 function ManagerItem({ data }: any) {
+  // 오픈 여부
+  const [isLocationOpen, setIsLocationOpen] = useState(false);
   const [isStatusOpen, setIsStatusOpen] = useState(false);
   const [isMemoOpen, setIsMemoOpen] = useState(false);
+  const [isVehicleOpen, setIsVehicleOpen] = useState(false);
 
-  let statusText;
-  let stateChangeButtons;
+  // 변경 클릭
+  const [isEditingName, setIsEditingName] = useState(false);
+  const [isEditingPhone, setIsEditingPhone] = useState(false);
+  const [isEditingLevel, setIsEditingLevel] = useState(false);
+  const [isEditingCertificate, setIsEditingCertificate] = useState(false);
+
+  // 활동 지역 외
+  const nameBlurHandler = () => {
+    setIsEditingName(false);
+    // 변경 api 요청
+  };
+
+  const phoneBlurHandler = () => {
+    setIsEditingPhone(false);
+    // 변경 api 요청
+  };
+
+  const levelBlurHandler = () => {
+    setIsEditingLevel(false);
+    // 변경 api 요청
+  };
+
+  const certificateBlurHandler = () => {
+    setIsEditingCertificate(false);
+    // 변경 api 요청
+  };
+
+  const vehicleChangeHandler = () => {
+    setIsVehicleOpen(false);
+  };
 
   const stateChangeHandler = () => {
     setIsStatusOpen(false);
@@ -17,6 +50,8 @@ function ManagerItem({ data }: any) {
     // 상태 변경 api 요청
   };
 
+  let statusText;
+  let stateChangeButtons;
   switch (data.managerStatus) {
     case '대기':
       statusText = '대기';
@@ -74,14 +109,35 @@ function ManagerItem({ data }: any) {
 
   return (
     <G.Tr>
-      <S.ManagerTd>{data.name}</S.ManagerTd>
-      <S.ManagerTd>010-0000-0000</S.ManagerTd>
+      <S.ManagerTd onClick={() => setIsEditingName(true)}>
+        {isEditingName ? <input type="text" onBlur={nameBlurHandler} value="홍길동" /> : data.name}
+      </S.ManagerTd>
+      <S.ManagerTd onClick={() => setIsEditingPhone(true)} onBlur={() => setIsEditingPhone(false)}>
+        {isEditingPhone ? <input type="text" onBlur={phoneBlurHandler} value="010-0000-1111" /> : '010-0000-1111'}
+      </S.ManagerTd>
       <S.ManagerTd>서울 특별시 서초대로 23-1</S.ManagerTd>
-      <S.ManagerTd>서울 특별시 서초대로 23-1</S.ManagerTd>
-      <S.ManagerTd>5</S.ManagerTd>
-      <S.ManagerTd>1급 (off)</S.ManagerTd>
+      <S.ManagerTd style={{ position: 'relative' }} onClick={() => setIsLocationOpen(true)}>
+        서울 금천구
+        {isLocationOpen && <LocationSelectionForm />}
+      </S.ManagerTd>
+      <S.ManagerTd onClick={() => setIsEditingLevel(true)}>
+        {isEditingLevel ? <input type="text" onBlur={levelBlurHandler} value="5" /> : '5'}
+      </S.ManagerTd>
+      <S.ManagerTd onClick={() => setIsEditingCertificate(true)}>
+        {isEditingCertificate ? <input type="text" onBlur={certificateBlurHandler} value="1급 (off)" /> : '1급 (off)'}
+      </S.ManagerTd>
       <S.ManagerTd>
-        <StateButton state={'green'}>가능</StateButton>
+        <StateButton state={'green'} onClick={() => setIsVehicleOpen(!isVehicleOpen)}>
+          가능
+        </StateButton>
+
+        {isVehicleOpen && (
+          <S.StateChangeContainer>
+            <StateButton state={'red'} onClick={vehicleChangeHandler}>
+              불가능
+            </StateButton>
+          </S.StateChangeContainer>
+        )}
       </S.ManagerTd>
       <S.ManagerTd>
         <StateButton state={'blue'}>지원폼</StateButton>
