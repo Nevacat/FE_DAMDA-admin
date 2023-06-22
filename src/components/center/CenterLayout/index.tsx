4299e3736cd6aef40493977f89bb3ae16a23fcc4
PYO SEUNGYEON <pyo.seungyeonn@gmail.com>
2023-06-22 13:33:28 +0900

    design: CenterLayout 컴포넌트 기본 스타일 구현

--- src/components/center/CenterLayout/index.tsx
+++ src/components/center/CenterLayout/index.tsx
@@ -1,7 +1,26 @@
 import React from 'react';
+import * as G from '@/styles/common/table.style';
+import * as S from './style';
 
 function CenterLayout() {
-  return <div>CenterLayout</div>;
+  return (
+    <div>
+      <S.Title>고객 센터</S.Title>
+
+      <G.TableContainer>
+        <G.Table>
+          <G.Tr>
+            <G.Th scope="col">순번</G.Th>
+            <G.Th scope="col">제목</G.Th>
+            <G.Th scope="col">유형</G.Th>
+            <G.Th scope="col">action</G.Th>
+          </G.Tr>
+
+          <G.Tbody></G.Tbody>
+        </G.Table>
+      </G.TableContainer>
+    </div>
+  );
 }
 
 export default CenterLayout;
---
+++ src/components/center/CenterLayout/style.ts
@@ -0,0 +1,8 @@
+import styled from '@emotion/styled';
+import theme from '@/styles/theme';
+
+export const Title = styled.h1`
+  margin-bottom: 65px;
+  color: ${theme.colors.yolda_black_1};
+  ${theme.font.bold_24_135}
+`;
