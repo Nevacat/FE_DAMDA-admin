e449212b003a21ade2049805a3f1bffc37c06daa
PYO SEUNGYEON <pyo.seungyeonn@gmail.com>
2023-06-22 19:25:49 +0900

    feat: postFAQData api 작성

--- src/api/center.ts
+++ src/api/center.ts
@@ -1,8 +1,12 @@
-import { CenterData } from '@/types/api/center';
-import axios from 'axios';
+import { CenterData, FAQData } from '@/types/api/center';
 import { instance } from './instance';
 
 export const getCenterData = async () => {
   const response = await instance.get<CenterData[]>('/member/qna');
+  return response.data;
+};
+
+export const postFAQData = async (formData: FAQData) => {
+  const response = await instance.post<boolean>('/admin/qna', formData);
   return response.data;
 };
