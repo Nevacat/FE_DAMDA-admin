641aaccb0b6657209884cce7d0111d67415e8b87
seunghye <tmdgp0212@naver.com>
2023-06-23 18:50:21 +0900

    design: 사이드메뉴 활성화

--- src/components/common/SideMenu/index.tsx
+++ src/components/common/SideMenu/index.tsx
@@ -3,9 +3,12 @@ import Link from 'next/link';
 import { BiChevronDown, BiChevronUp } from 'react-icons/bi';
 import * as S from './style';
 import Image from 'next/image';
+import { NextRouter, useRouter } from 'next/router';
 
-function SideMenu() {
+function SideMenu({ router }: { router: NextRouter }) {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const pathname = router.pathname;
+  console.log(router);
 
   return (
     <S.SideBar>
@@ -13,35 +16,63 @@ function SideMenu() {
         <Image src="/icons/Yolda_logo.svg" alt="열다" width={53} height={25} />
       </S.Logo>
       <S.Nav>
-        <Link href={'/'}>
-          <Image src="/icons/menu-reserve.svg" alt="예약관리" width={24} height={24} />
+        <Link href={'/'} className={pathname === '/' ? 'active' : ''}>
+          {pathname === '/' ? (
+            <Image src="/icons/menu-reserve-w.svg" alt="예약관리" width={24} height={24} />
+          ) : (
+            <Image src="/icons/menu-reserve.svg" alt="예약관리" width={24} height={24} />
+          )}
           예약 관리
         </Link>
-        <Link href={'/user'}>
-          <Image src="/icons/menu-user.svg" alt="고객관리" width={24} height={24} />
+        <Link href={'/user'} className={pathname === '/user' ? 'active' : ''}>
+          {pathname === '/user' ? (
+            <Image src="/icons/menu-user-w.svg" alt="고객관리" width={24} height={24} />
+          ) : (
+            <Image src="/icons/menu-user.svg" alt="고객관리" width={24} height={24} />
+          )}
           고객 관리
         </Link>
-        <Link href={'/manager'}>
-          <Image src="/icons/menu-manager.svg" alt="매니저관리" width={24} height={24} />
+        <Link href={'/manager'} className={pathname === '/manager' ? 'active' : ''}>
+          {pathname === '/manager' ? (
+            <Image src="/icons/menu-manager-w.svg" alt="매니저관리" width={24} height={24} />
+          ) : (
+            <Image src="/icons/menu-manager.svg" alt="매니저관리" width={24} height={24} />
+          )}
           매니저 관리
         </Link>
-        <Link href={'/form'}>
-          <Image src="/icons/menu-form.svg" alt="폼 관리" width={24} height={24} />
+        <Link href={'/form'} className={pathname === '/form' ? 'active' : ''}>
+          {pathname === '/form' ? (
+            <Image src="/icons/menu-form-w.svg" alt="폼 관리" width={24} height={24} />
+          ) : (
+            <Image src="/icons/menu-form.svg" alt="폼 관리" width={24} height={24} />
+          )}
           고객 예약 폼
         </Link>
         <S.Folder>
-          <div className="category" onClick={() => setIsMenuOpen(!isMenuOpen)}>
-            <Image src="/icons/menu-posts.svg" alt="게시물 관리" width={24} height={24} />
+          <div
+            className={`category ${
+              pathname === '/review' || pathname === '/review/create' || pathname === '/center' ? 'active' : ''
+            }`}
+            onClick={() => setIsMenuOpen(!isMenuOpen)}
+          >
+            {pathname === '/review' || pathname === '/review/create' || pathname === '/center' ? (
+              <Image src="/icons/menu-posts-w.svg" alt="게시물 관리" width={24} height={24} />
+            ) : (
+              <Image src="/icons/menu-posts.svg" alt="게시물 관리" width={24} height={24} />
+            )}
             게시물 관리
             {isMenuOpen ? <BiChevronUp /> : <BiChevronDown />}
           </div>
           {isMenuOpen && (
             <div className="sub_menu">
-              <Link href={'/review'}>
+              <Link
+                href={'/review'}
+                className={pathname === '/review/create' || pathname === '/review' ? 'active' : ''}
+              >
                 <Image src="/icons/menu-review.svg" alt="고객 후기" width={24} height={24} />
                 고객 후기
               </Link>
-              <Link href={'/center'}>
+              <Link href={'/center'} className={pathname === '/center' ? 'active' : ''}>
                 <Image src="/icons/menu-center.svg" alt="고객센터" width={24} height={24} />
                 고객 센터
               </Link>
