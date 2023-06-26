cd8b9fa6236cbafdb0ae8ceaeba41b06f7bce9ac
seunghye <tmdgp0212@naver.com>
2023-06-26 13:13:57 +0900

    design: 각 페이지 타이틀 폰트사이즈 조절

--- src/styles/common/PageTitle.ts
+++ src/styles/common/PageTitle.ts
@@ -2,6 +2,6 @@ import styled from '@emotion/styled';
 import theme from '../theme';
 
 export const PageTitle = styled.h2`
-  ${theme.font.bold_24}
+  ${theme.font.bold_18_135}
   cursor: default;
 `;
