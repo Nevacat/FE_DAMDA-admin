83261661dd2742d7e657b2dd97758d7639b8322d
Nevacat <nevacattery217@naver.com>
2023-06-28 09:11:18 +0900

    feat: 예약관리 페이지에 History팝업 추가

--- src/components/main/MatchingPopup/TableItem/index.tsx
+++ src/components/main/MatchingPopup/TableItem/index.tsx
@@ -3,6 +3,7 @@ import * as T from '@/styles/common/table.style';
 import { CertificateEnum, MatchingData, MatchingStatusEnum } from '@/types/api/main';
 import * as S from './style';
 import { formatDate } from '../../DateFormat';
+import History from '@/components/common/History';
 
 interface TableItemProps {
   data: MatchingData;
@@ -16,34 +17,45 @@ function TableItem({ data, isChecked, isCheckboxDisabled, onCheckboxChange }: Ta
   const certificate = CertificateEnum[data.certificate];
   const shouldHideCheckbox = data.matchingResponse === 'NO';
 
+  const [isOpen, setIsOpen] = React.useState(false);
+
+  const handleHistoryClick = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    event.stopPropagation();
+    event.preventDefault();
+    setIsOpen(!isOpen);
+  };
+
   console.log(data.updateAt);
   return (
-    <T.Tr>
-      <T.Td>
-        <input
-          type="checkbox"
-          checked={isChecked}
-          disabled={isCheckboxDisabled}
-          onChange={(event) => onCheckboxChange(event, data.matchId)}
-          style={{ display: shouldHideCheckbox ? 'none' : 'initial' }}
-        />
-      </T.Td>
-      <T.Td>
-        <S.ResState state={data.matchingResponse}>{resState}</S.ResState>
-      </T.Td>
-      <T.Td>{data.updateAt}</T.Td>
-      <T.Td>{data.name}</T.Td>
-      <T.Td>{data.phone}</T.Td>
-      <T.Td>{data.activityArea}</T.Td>
-      <T.Td>{data.level}</T.Td>
-      <T.Td>{certificate}</T.Td>
-      <T.Td>
-        <S.ResState state={data.driving}>{data.driving ? 'O' : 'X'}</S.ResState>
-      </T.Td>
-      <T.Td>
-        <S.HistoryButton>예약 히스토리</S.HistoryButton>
-      </T.Td>
-    </T.Tr>
+    <>
+      <T.Tr>
+        <T.Td>
+          <input
+            type="checkbox"
+            checked={isChecked}
+            disabled={isCheckboxDisabled}
+            onChange={(event) => onCheckboxChange(event, data.matchId)}
+            style={{ display: shouldHideCheckbox ? 'none' : 'initial' }}
+          />
+        </T.Td>
+        <T.Td>
+          <S.ResState state={data.matchingResponse}>{resState}</S.ResState>
+        </T.Td>
+        <T.Td>{data.updateAt}</T.Td>
+        <T.Td>{data.name}</T.Td>
+        <T.Td>{data.phone}</T.Td>
+        <T.Td>{data.activityArea}</T.Td>
+        <T.Td>{data.level}</T.Td>
+        <T.Td>{certificate}</T.Td>
+        <T.Td>
+          <S.ResState state={data.driving}>{data.driving ? 'O' : 'X'}</S.ResState>
+        </T.Td>
+        <T.Td>
+          <S.HistoryButton onClick={handleHistoryClick}>예약 히스토리</S.HistoryButton>
+        </T.Td>
+      </T.Tr>
+      {isOpen && <History type="MANAGER" userId={data.managerId} username={data.name} setIsOpen={setIsOpen} />}
+    </>
   );
 }
 
